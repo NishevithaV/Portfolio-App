a45907aa5e3593d8ed458d12d8de9e8fe7458025
Nishevitha <nishevitha.v@gmail.com>
2025-01-07 21:34:49 -0400

    first draft of work experience section completed

---
+++ app/constants/index.js
@@ -0,0 +1,23 @@
+export const WORKEXPERIENCE = [
+    {
+        year:"May 2024 – Aug 2024",
+        role:"Software Developer Intern",
+        company:"Silver Arrow Logistics Solutions Pvt Ltd.",
+        description: `Engineered a middleware API to enhance real-time data processing, increasing throughput by 200 transactions per minute through RESTful API integration and rigorous testing with Postman. Designed AWS infrastructure with EC2 instances, boosting deployment efficiency by 40% and ensuring system reliability. Re-engineered data migration to DynamoDB with Drools rules, ensuring data integrity.`,
+        skills: ["AWS EC2","RESTful APIs","DynamoDB", "Postman", "Drools"],
+    },
+    {
+        year:"June 2024 – Aug 2024",
+        role:"Freelance Web Developer",
+        company:"Preserving My Good Self",
+        description: `Built a dynamic research portfolio website from scratch as a freelance project, using ReactJS for an interactive front-end and Strapi CMS for content management. Collaborated with a UI/UX designer to enhance user experience, reducing page load times by 1 second through responsive design optimizations. Deployed the site, automating the deployment pipeline to cut deployment time by 60% and seamlessly integrating the domain name.`,
+        skills: ["React.js", "CMS", "REST APIs", "HTML5", "CSS"],
+    },
+    {
+        year:"Feb 2024 – Present",
+        role:"Student Ambassador",
+        company:"ShiftKey Labs",
+        description: `Developed and led the Sandbox’s annual incubator program, securing 10 industry-led sessions and events to enhance project outcomes and entrepreneurial impact. Managed timelines and resources for startup teams, ensuring smooth progress from ideation to pitch presentations. Collaborated with the Faculty of Computer Science to organize large-scale tech events, including hackathons and workshops, engaging over 3,500 students.`,
+        skills: ["Program Development", "Event Coordination", "Resource Management", "Team Collaboration"],
+    },
+];
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Navbar from './components/Navbar'
+import WorkSection from "./components/WorkSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto px-12 py-4">
         <Hero />
+        <WorkSection />
       </div>
     </main>
   );
