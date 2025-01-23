fb3e08abb9a5c3ddc42a5afd918c618bee42a39e
Nishevitha <nishevitha.v@gmail.com>
2025-01-22 23:51:47 -0400

    addedoverlay on project cards for links

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -4,12 +4,14 @@ const ProjectCard = ({ id, title, desc, img, iconLists, link }) => {
   return (
     <div>
         <div 
-            className="h-52 md:h-72"
+            className="h-52 md:h-72 rounded-t-xl relative group"
             style={{backgroundImage: `url(${img})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat",}}>
+              <div className="overlay absolute top-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500"></div>
         </div>
+        
         <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
-            <h5>{title}</h5>
-            <p>{desc}</p>
+            <h5 className="font-xl font-semibold mb-2">{title}</h5>
+            <p className="text-neutral-400">{desc}</p>
         </div>
     </div>
   )
