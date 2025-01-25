c08659aaf2b6286b517ce004f7d055ebf80ba20a
Nishevitha <nishevitha.v@gmail.com>
2025-01-25 12:10:41 -0400

    added grid layout to project cards

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/outline'
+import { CodeBracketIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 
-const ProjectCard = ({ id, title, desc, img, iconLists, link }) => {
+const ProjectCard = ({ title, desc, img, iconLists, link }) => {
   return (
-    <div>
+    <div className="flex flex-col justify-between bg-[#181818] rounded-xl overflow-hidden min-h-[400px]">
         <div 
             className="h-52 md:h-72 rounded-t-xl relative group"
             style={{backgroundImage: `url(${img})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat",}}>
               <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 items-center justify-center">
-                <Link href={link} className="h-14 w-14 mr-2 border-2 relative rounded-full border-neutral-400 hover:border-white flex items-center justify-center group/link">
+                <Link href={link} target="_blank" rel="noopener noreferrer" className="h-14 w-14 mr-2 border-2 relative rounded-full border-neutral-400 hover:border-white flex items-center justify-center group/link">
                   <CodeBracketIcon className="h-10 w-10 text-neutral-400 group-hover/link:text-white" />
                 </Link>
-
-                {/* <Link href="/" className="h-14 w-14 border-2 relative rounded-full border-neutral-400 hover:border-white flex items-center justify-center group/link">
-                  <EyeIcon className="h-10 w-10 text-neutral-400 group-hover/link:text-white" />
-                </Link> */}
               </div>
           </div>
         
-        <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
-            <h5 className="font-xl font-semibold mb-2">{title}</h5>
-            <p className="text-neutral-400">{desc}</p>
+        <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4 flex flex-col flex-grow justify-between">
+          <div>
+              <h5 className="font-xl font-semibold mb-2">{title}</h5>
+              <p className="text-neutral-400">{desc}</p>
+            </div>
             <div className="flex gap-2 mt-4">
                   {iconLists.map((icon, index) => (
                     <img key={index} src={icon} alt="icon" className="h-5 w-5" />
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -5,10 +5,10 @@ import ProjectCard from './ProjectCard'
 const Projects = () => {
   return (
     <div className="py-20">
-        <h1 className="text-2xl sm:text-3xl text-white text-center pb-8">Recent Projects
+        <h1 className="text-2xl sm:text-3xl text-white text-center mb-10">Recent Projects
         </h1>
         
-        <div>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12">
           {PROJECTS.map((project) => (
             <ProjectCard key={project.id} title={project.title} desc={project.desc} img={project.img} iconLists={project.iconLists} link={project.link} />
           ))}
