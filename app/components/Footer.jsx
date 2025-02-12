1abf5f15b131a5b31db15efa32637c186c41b940
Nishevitha <nishevitha.v@gmail.com>
2025-02-11 23:57:57 -0400

    added footer logo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
 
   return (
     <footer className="footer border border-l-transparent border-r-transparent border-b-transparent border-t-transparent text-white">
-        <div className="container p-12 pb-8">
+        <div className="container p-12 pb-8 flex justify-between">
+          <span><img src="/images/footer_logo.png" alt="logo footer" className="w-auto h-10 object-contain" /></span>
             <p className="text-xs">&copy; {getYear()} Nishevitha Venkatesh. <br /> All rights reserved.</p>
         </div>
     </footer>
