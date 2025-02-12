1d86831871e37a8d198a05fa496e20abb7f37093
Nishevitha <nishevitha.v@gmail.com>
2025-02-11 23:59:06 -0400

    completed footer design changes

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   const getYear = () => new Date().getFullYear();
 
   return (
-    <footer className="footer border border-l-transparent border-r-transparent border-b-transparent border-t-transparent text-white">
-        <div className="container p-12 pb-8 flex justify-between">
+    <footer className="footer border border-l-transparent border-r-transparent border-b-transparent text-white">
+        <div className="container p-12 pb-8 pt-8 flex justify-between">
           <span><img src="/images/footer_logo.png" alt="logo footer" className="w-auto h-10 object-contain" /></span>
             <p className="text-xs">&copy; {getYear()} Nishevitha Venkatesh. <br /> All rights reserved.</p>
         </div>
