d6fc94f67e79aeaf5a47f8f02f3d6c66c3daa97f
Nishevitha <nishevitha.v@gmail.com>
2025-02-12 00:03:31 -0400

    updated layout between contact section and footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const getYear = () => new Date().getFullYear();
 
   return (
-    <footer className="footer border border-l-transparent border-r-transparent border-b-transparent text-white">
+    <footer className="footer border border-l-transparent border-r-transparent border-b-transparent text-white mt-12">
         <div className="container p-12 pb-8 pt-8 flex justify-between">
           <span><img src="/images/footer_logo.png" alt="logo footer" className="w-auto h-10 object-contain" /></span>
             <p className="text-xs">&copy; {getYear()} Nishevitha Venkatesh. <br /> All rights reserved.</p>
