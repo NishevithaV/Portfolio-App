4ce60843072a62024cda0fb59ab44a832df3ee5a
Nishevitha <nishevitha.v@gmail.com>
2025-02-13 23:58:56 -0400

    modified POST request

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,17 +5,22 @@ import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(request) {
+export async function POST(request, res) {
+  const { body } = request;
+  const { email, subject, message } = body;
   try {
-    
     const { data, error } = await resend.emails.send({
-      from: "Nishevitha <nishevitha.v@gmail.com>",
-      to: ["nishevitha.v@gmail.com"], 
-      subject: "Hello World",
+      from: fromEmail,
+      to: [email], 
+      subject: subject,
       react: (
         <div style={{ fontFamily: "Arial, sans-serif", lineHeight: "1.5" }}>
-          <p>This is the email body content.</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
         </div>
       ),
     });
