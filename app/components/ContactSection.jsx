80ab65f30067eff8ebb781e59d7e08ba61fd5d0e
Nishevitha <nishevitha.v@gmail.com>
2025-03-13 21:51:47 -0300

    completed contact form

--- app/components/ContactSection.jsx
+++ app/components/ContactSection.jsx
@@ -7,23 +7,23 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 const ContactSection = () => {
   return (
     
-    <section className="flex flex-col md:flex-row items-center justify-between gap-8 text-center mt-4 w-full max-w-4xl mx-auto md:text-right md:gap-12" id="contact">
+    <section className="flex flex-col items-center justify-center text-center mb-4 gap-6 mt-4 w-full max-w-4xl mx-auto md:flex-row md:justify-between md:text-right md:gap-12" id="contact">
 
-        <div className="w-full md:w-1/2 flex flex-col items-end md:max-w-lg">
-            <h4 className="text-2xl font-bold text-white">Let's Connect</h4>
+        <div className="w-full md:w-1/2 flex flex-col items-center md:items-end">
+            <h4 className="text-3xl font-bold text-white mb-2">Let's Connect</h4>
             <p className="text-neutral-400 text-base sm:text-lg mb-3 mt-2 md:mt-1">
                 {" "}
                 Have questions, ideas or opportunities? I'm excited to hear from you. Send me a message!
             </p>
         </div>
 
-        <div className="w-full md:w-1/2 flex justify-start">
-          <div className="flex flex-row gap-4 mt-2 md:mt-0">
+        <div className="w-full md:w-1/2 flex justify-center md:justify-start">
+          <div className="flex flex-row gap-5 mt-0 md:mt-0 md:gap-6 lg:gap-7">
               <a href="mailto:nishevitha.v@gmail.com" className="p-2 rounded-full bg-gradient-to-r from-teal-400 to-fuchsia-400 hover:scale-110 transition-transform duration-200">
-                <FontAwesomeIcon icon={faPaperPlane} className="text-white" style={{ width: '50px', height: '50px' }} />
+                <FontAwesomeIcon icon={faPaperPlane} className="text-white w-10 h-10 md:w-12 md:h-12" />
               </a>
               <a href="https://www.linkedin.com/in/nishevitha-venkatesh/" target="_blank" rel="noopener nonreferrer" className="p-2 rounded-full bg-gradient-to-r from-teal-400 to-fuchsia-400 hover:scale-110 transition-transform duration-200">
-                <FontAwesomeIcon icon={faLinkedin} className="text-white" style={{ width: '50px', height: '50px' }} />
+                <FontAwesomeIcon icon={faLinkedin} className="text-white w-10 h-10 md:w-12 md:h-12" />
               </a>
             </div>  
         </div>
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="footer border border-l-transparent border-r-transparent border-b-transparent text-white mt-12">
-        <div className="container p-12 pb-8 pt-8 flex justify-between">
+        <div className="container p-12 pb-8 pt-7 flex justify-between">
           <span><img src="/images/footer_logo.png" alt="logo footer" className="w-auto h-10 object-contain" /></span>
             <p className="text-xs">&copy; {getYear()} Nishevitha Venkatesh. <br /> All rights reserved.</p>
         </div>
