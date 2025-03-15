87365df313675619fbf4b2c821a2f21ecc837fcd
Nishevitha <nishevitha.v@gmail.com>
2025-03-14 23:34:09 -0300

    updated footer design

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,9 +5,13 @@ const Footer = () => {
 
   return (
     <footer className="footer border border-l-transparent border-r-transparent border-b-transparent text-white mt-12">
-        <div className="container p-12 pb-8 pt-7 flex justify-between">
-          <span><img src="/images/footer_logo.png" alt="logo footer" className="w-auto h-10 object-contain" /></span>
-            <p className="text-xs">&copy; {getYear()} Nishevitha Venkatesh. <br /> All rights reserved.</p>
+        <div className="container p-8 flex justify-between items-center w-full">
+
+          <p className="text-white font-semibold text-2xl text-left">NV</p>
+
+          {/* <span><img src="/images/footer_logo.png" alt="logo footer" className="w-auto h-10 object-contain" /></span> */}
+
+            <p className="text-xs text-right">&copy; {getYear()} Nishevitha Venkatesh. <br /> All rights reserved.</p>
         </div>
     </footer>
   )
